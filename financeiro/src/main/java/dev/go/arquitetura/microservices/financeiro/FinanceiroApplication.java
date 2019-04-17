a93b23703e6c7415e905ec72023d3896795e7d22
Marcos Buganeme <marcos.after@gmail.com>
2019-04-17 17:37:08 -0300

    Módulo financeiro
            Faz parte do escopo desse projeto:
                * Carregar toda regra de negócio específica referente ao módulo financeiro
                * Expor uma Api Restfull consistente
                * Testes unitários referentes as regras
                * Garantia de testes de integração

---
+++ financeiro/src/main/java/dev/go/arquitetura/microservices/financeiro/FinanceiroApplication.java
@@ -0,0 +1,13 @@
+package dev.go.arquitetura.microservices.financeiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceiroApplication.class, args);
+	}
+
+}
