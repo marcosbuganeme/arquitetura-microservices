04575d1691be085ecda20a633e1e0fa048f784e9
Marcos Buganeme <marcos.after@gmail.com>
2019-04-17 21:20:55 -0300

    gateway
    * mapeando porta do serviço 8083
    * apontando o servidor eureka 8081
    * criando rota para a base de dados jpa
    * configurando banco de dados
    * configurando jwt assinado

--- gateway/src/main/java/dev/go/arquitetura/microservices/gateway/GatewayApplication.java
+++ gateway/src/main/java/dev/go/arquitetura/microservices/gateway/GatewayApplication.java
@@ -2,12 +2,17 @@ package dev.go.arquitetura.microservices.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+
+import dev.go.arquitetura.microservices.core.security.JwtConfiguration;
 
 @SpringBootApplication
+@ComponentScan("dev.go.arquitetura.microservices")
+@EnableConfigurationProperties(value = JwtConfiguration.class)
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-
 }
---
+++ gateway/src/main/java/dev/go/arquitetura/microservices/gateway/config/EurekaConfig.java
@@ -0,0 +1,8 @@
+package dev.go.arquitetura.microservices.gateway.config;
+
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEurekaClient
+public class EurekaConfig {}
