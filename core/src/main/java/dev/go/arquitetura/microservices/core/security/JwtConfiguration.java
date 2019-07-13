24c2182d4c9f9ba883f72d103f3ed04d699305e0
Marcos Buganeme <marcos.after@gmail.com>
2019-07-13 15:42:13 -0300

    modificando forma de disponibilização de acesso aos dados de configuração de um json web token

--- core/src/main/java/dev/go/arquitetura/microservices/core/security/JwtConfiguration.java
+++ core/src/main/java/dev/go/arquitetura/microservices/core/security/JwtConfiguration.java
@@ -4,20 +4,15 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.context.annotation.Configuration;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
-
-@Getter
-@Setter
-@ToString
 @Configuration
 @ConfigurationProperties(prefix = "jwt.config")
 public class JwtConfiguration {
 
-	private int expiration = 3600;
-	private String loginUrl = "/login/**";
-	private String type = "encrypted";
-	private String privateKey = "qxBEEQv7E8aviX1KUcdOiF5ve5COUPAr";
-	private @NestedConfigurationProperty Header header = new Header();
+	public static final int expiration = 3600;
+	public static final String type = "encrypted";
+	public static final String loginUrl = "/auth/**";
+	public static final String privateKey = "qxBEEQv7E8aviX1KUcdOiF5ve5COUPAr";
+
+	@NestedConfigurationProperty
+	public static final Header header = new Header();
 }
